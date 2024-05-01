d215bac09b9371f9a3aba5b38815dac0237cb8b5
Rodriasd <veronrodrigo98@gmail.com>
2024-05-01 13:07:27 -0300

    added the interfaces of the diaries

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,2 +1,14 @@
 export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'story'
 export type Visibility = 'great' | 'good' | 'ok' | 'poor'
+
+export interface DiaryEntry {
+    id: number,
+    data: string,
+    weather: Weather,
+    visibility: Visibility,
+    comment: string
+}
+
+// interface SpecialEntry extends DiaryEntry {
+//     flightNumber: number
+// }
